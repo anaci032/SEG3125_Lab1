02e4a0b1f9ab5c102426a13be0e061341548aa80
anaci032 <65175691+anaci032@users.noreply.github.com>
2024-01-16 00:48:56 -0500

    v1

---
+++ Lab1.js
@@ -0,0 +1,4 @@
+document.getElementById("beginSurveyBtn").addEventListener("click", function() {
+    var additionalText = document.getElementById("additionalText");
+    additionalText.classList.toggle("hidden");
+});
